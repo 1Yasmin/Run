974594ded4234ced200360c97b45172f5485fa86
Cpzty <Cristianpzty@gmail.com>
2018-09-25 08:42:40 -0600

    scene 1 p1,p2 movement

---
+++ Assets/Scripts/Player2Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Movement : MonoBehaviour {
+public GameObject p1;
+
+public float Speed = 0f;
+private float movex = 1f;
+private float movey = 1f;
+
+// Use this for initialization
+void Start () {
+}
+
+// Update is called once per frame
+void FixedUpdate () {
+	if(p1){
+		movex = Input.GetAxis ("P2_horizontal");
+		movey = Input.GetAxis ("P2_vertical");
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (movex * Speed, movey * Speed);
+
+	}
+
+
+}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+public GameObject p1;
+
+public float Speed = 0f;
+private float movex = 1f;
+private float movey = 1f;
+
+// Use this for initialization
+void Start () {
+}
+
+// Update is called once per frame
+void FixedUpdate () {
+	if(p1){
+		movex = Input.GetAxis ("P1_horizontal");
+		movey = Input.GetAxis ("P1_vertical");
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (movex * Speed, movey * Speed);
+
+	}
+
+
+}
+}
